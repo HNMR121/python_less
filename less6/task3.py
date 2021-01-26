20fa05d1b8c7f1ad2e6bfde1f34ea80b4c0fc76d
hits <alexf1@list.ru>
2021-01-26 16:04:59 +0300

    task3 ok

---
+++ less6/task3.py
@@ -0,0 +1,23 @@
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + " " + self.surname
+
+    def get_total_income(self):
+        return self._income.get('wage')+self._income.get('bonus')
+
+
+position = Position("Alexander", "Ivanov", "engineer", 10000, 5000)
+print(position.get_full_name())
+print(position.get_total_income())
